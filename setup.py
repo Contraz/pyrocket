518d2cc5b0221cba710e6f6d4dd4377e0cabd02d
Einar Forselv <eforselv@gmail.com>
2017-04-14 22:32:25 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyrocket",
-    version="0.1.0",
+    version="0.1.1",
     description="Rocket sync-tracker client",
     long_description=open('README.rst').read(),
     url="https://github.com/Contraz/pyrocket",
